bfe7ab74b6a15170addd0568ebe504fe5461202a
RahulGorai0206 <rahulgorai0206@gmail.com>
2023-11-28 01:59:39 +0530

    Fix add vehicle and driver page also improve logic

--- pages/js/addDriver.js
+++ pages/js/addDriver.js
@@ -1,38 +1,43 @@
 function submitForm() {
-    // Get form data
-    const fullName = document.getElementById('fullName').value;
-    const phoneNumber = document.getElementById('phoneNumber').value;
-    const aadharNumber = document.getElementById('aadharNumber').value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
-    const age = document.getElementById('age').value;
-    const photographURL = document.getElementById('photographURL').value;
-  
-    // Create data object
-    const formData = {
-      driverId:aadharNumber,
+  // Get form data
+  const fullName = document.getElementById('fullName').value;
+  const phoneNumber = document.getElementById('phoneNumber').value;
+  const aadharNumber = document.getElementById('aadharNumber').value;
+  const gender = document.querySelector('input[name="gender"]:checked');
+  const age = document.getElementById('age').value;
+  const photographURL = document.getElementById('photographURL').value;
+
+  // Check if any required field is empty
+  if (!fullName || !phoneNumber || !aadharNumber || !gender || !age || !photographURL) {
+      alert('Please fill in all required fields.');
+      return; 
+  }
+
+  const selectedGender = gender.value;
+
+  // Create data object
+  const formData = {
+      driverId: aadharNumber,
       name: fullName,
       phoneNumbers: phoneNumber,
       age: age,
-      gender: gender,
+      gender: selectedGender,
       image: photographURL
-    };
-  
-    // Make a POST request to the API endpoint
-    fetch('https://garbage-collect-backend.onrender.com/send-driver', {
+  };
+
+  fetch('https://garbage-collect-backend.onrender.com/send-driver', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
       },
       body: JSON.stringify(formData),
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-      // Handle success, e.g., show a success message to the user
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      console.log('Failed Data:', formData);
-      // Handle errors, e.g., show an error message to the user
-    });
-  }
+  })
+      .then(response => response.json())
+      .then(data => {
+          console.log('Success:', data);
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+          console.log('Failed Data:', formData);
+      });
+}

9d0ca8c8dced2085da2ce35c8bd12f7134575562
RahulGorai0206 <rahulgorai0206@gmail.com>
2023-11-29 22:26:11 +0530

    check status every second

--- pages/js/TrackingPage.js
+++ pages/js/TrackingPage.js
@@ -13,56 +13,73 @@ function fetchDustbinsData() {
         });
 }
 
+// Function to update the dustbin tracking UI
+function updateTrackingUI(data) {
+    // Find the area with the given areaId
+    const area = data.find(area => area.areaId === areaId);
+
+    if (area) {
+        // Dynamically add cards based on the dustbins in the area
+        let trackContainer = document.getElementById('trackContainer');
+        trackContainer.innerHTML = ''; // Clear previous content
+
+        area.dustbins.forEach((dustbin, index) => {
+            // Create a card element
+            let card = document.createElement('div');
+            card.className = 'card';
+
+            // Create status elements
+            let statusContainer = document.createElement('div');
+            statusContainer.className = 'status';
+
+            let arrivedStatus = document.createElement('span');
+            arrivedStatus.className = 'arrived';
+            arrivedStatus.textContent = dustbin.isVisited ? 'Arrived' : 'Not Arrived Yet';
+
+            statusContainer.appendChild(arrivedStatus);
+
+            // Create circle element
+            let circle = document.createElement('div');
+            circle.className = 'circle';
+            circle.style.backgroundColor = dustbin.isVisited ? 'green' : 'gray';
+
+            // Append status and circle elements to the card
+            card.appendChild(statusContainer);
+            card.appendChild(circle);
+
+            // Append the card to the track container
+            trackContainer.appendChild(card);
+
+            // Log the dustbin coordinates and isVisited status
+            console.log(`Dustbin ${index + 1} Coordinates:`, dustbin.coordinates);
+            console.log(`Dustbin ${index + 1} isVisited:`, dustbin.isVisited);
+        });
+
+    } else {
+        console.error('Area not found with areaId:', areaId);
+    }
+}
+
 // Get areaId from the URL
 const areaId = getQueryParameter('areaId');
 
-// Fetch dustbins data from the API
+// Fetch the initial dustbins data
 fetchDustbinsData()
     .then(data => {
-        // Find the area with the given areaId
-        const area = data.find(area => area.areaId === areaId);
-
-        if (area) {
-            // Dynamically add cards based on the dustbins in the area
-            let trackContainer = document.getElementById('trackContainer');
-
-            area.dustbins.forEach((dustbin, index) => {
-                // Create a card element
-                let card = document.createElement('div');
-                card.className = 'card';
-
-                // Create status elements
-                let statusContainer = document.createElement('div');
-                statusContainer.className = 'status';
-
-                let arrivedStatus = document.createElement('span');
-                arrivedStatus.className = 'arrived';
-                arrivedStatus.textContent = dustbin.isVisited ? 'Arrived' : 'Not Arrived Yet';
-
-                statusContainer.appendChild(arrivedStatus);
-
-                // Create circle element
-                let circle = document.createElement('div');
-                circle.className = 'circle';
-                circle.style.backgroundColor = dustbin.isVisited ? 'green' : 'gray';
-
-                // Append status and circle elements to the card
-                card.appendChild(statusContainer);
-                card.appendChild(circle);
-
-                // Append the card to the track container
-                trackContainer.appendChild(card);
-
-                // Log the dustbin coordinates and isVisited status
-                console.log(`Dustbin ${index + 1} Coordinates:`, dustbin.coordinates);
-                console.log(`Dustbin ${index + 1} isVisited:`, dustbin.isVisited);
-            });
-
-        } else {
-            console.error('Area not found with areaId:', areaId);
-        }
+        updateTrackingUI(data);
     })
     .catch(error => console.error('Error fetching data:', error));
+
+// Set up interval to fetch data every 1 second
+setInterval(() => {
+    fetchDustbinsData()
+        .then(data => {
+            updateTrackingUI(data);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}, 1000);
+
+// ... (rest of the code remains unchanged)
 
 function getCookie(name) {
     var nameEQ = name + "=";

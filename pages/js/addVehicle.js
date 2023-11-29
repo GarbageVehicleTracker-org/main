c38accd95302883ca64d8915da44d5180898a870
RahulGorai0206 <rahulgorai0206@gmail.com>
2023-11-29 15:31:56 +0530

    Add login validation to all pages

--- pages/js/addVehicle.js
+++ pages/js/addVehicle.js
@@ -31,18 +31,56 @@ function sendDataToBackend(data) {
         },
         body: JSON.stringify(data),
     })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(responseData => {
-        alert('Data saved successfully!');
-        console.log(responseData);
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        alert('Failed to save data. Please try again.');
-    });
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(responseData => {
+            alert('Data saved successfully!');
+            console.log(responseData);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+            alert('Failed to save data. Please try again.');
+        });
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
+}
+
+function logout() {
+    deleteCookie("authToken");
+    window.location.href = "../index.html";
+}
+
+window.onload = function () {
+    var authToken = getCookie("authToken");
+    if (!authToken) {
+        window.location.href = "../index.html";
+    }
+};
+
+function redirectToHome() {
+    window.location.href = 'home.html';
 }
